3e58cce561b328e8729a5d0684f5a5e8736f99a3
thelowlevelguy <gkalla013@gmail.com>
2026-07-03 21:24:11 +0000

    feat: add operate funtion

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const sub = (a, b) => a - b;
 const mul = (a ,b) => a * b;
 const div = (a, b) => b > 0 ? a / 0 : a;
 
+const operate = (op, num1, num2) => {
+	switch(op){
+	case "+":
+		return add(a,b);
+	case "-":
+		return sub(a,b);
+	case "*":
+		return mul(a,b);
+	case "/":
+		return div(a,b);
+	}
+}
 
-const operate = (op, num1, num2)
